720613631838316336f7b57167ef312a23729805
KarolRzczkowski <rzeczkowskikarol166@gmail.com>
2023-09-02 10:40:01 +0200

    seven commit

--- client/app/components/Firstpage.tsx
+++ client/app/components/Firstpage.tsx
@@ -6,13 +6,13 @@ import github from './images/github.svg';
 import google from './images/google.svg';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, googleprovider, githubprovider } from '../config/firebaseconfig';
-import { useMemo } from 'react';
 import { usePathname } from 'next/navigation';
-import spotify from './images/spotify-icon.svg'
+import spotify from './images/spotify-icon.svg';
 import { useRouter } from 'next/navigation';
-import ForgottenPassword from '../password/ForgottenPassword';
+import userIcon from './images/user-128.svg';
 import Link from 'next/link';
-import password from '../password/ForgottenPassword';
+import { AiOutlineUser } from 'react-icons/ai';
+
 interface FirstPageProps {
   isLoggedin?: boolean;
 }
@@ -24,10 +24,10 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
   const [isloggedin, setIsloggedin] = useState<boolean>(false);
   const [isgithubloggedin, setIsGithubLoggedin] = useState<boolean>(false);
   const [showLoginForm, setShowLoginForm] = useState<boolean>(false);
-
-   //creating usememo navigation
-   const router   = useRouter()
-   const pathname = usePathname()
+  const [showuserdetails, setShowuserdetails] = useState<boolean>(false);
+  const [generate , setGenerate] = useState("")
+  const router = useRouter();
+  const pathname = usePathname();
 
   const handlegoogleclick = () => {
     signInWithPopup(auth, googleprovider)
@@ -93,7 +93,7 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
         console.log('Email and password cannot be empty');
         return;
       }
-  
+
       const response = await axios.post('/auth', { email, password });
       if (response.status === 200) {
         console.log('Login successful');
@@ -107,7 +107,6 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
       console.log('Error during login:', error);
     }
   };
-  
 
   const handlereload = () => {
     window.location.reload();
@@ -115,36 +114,60 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
     setIsGithubLoggedin(false);
     setShowLoginForm(true);
   };
+
+  const handleUserIconClick = () => {
+    setShowuserdetails(!showuserdetails);
+  };
+
+  //password generator
+  const Generatepassword = () => {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>? ";
+    let password = '';
+
+    for (let i = 0; i < 12; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+
+    return password;
+  }
+
+  const handlegeneratepassword = () => {
+    const generatedPassword = Generatepassword();
+    setPassword(generatedPassword); // Ustawia wygenerowane hasło w stanie `password`
+    console.log(generatedPassword);
+  }
+
+  if(generate.length <= 12 ){
+    try {
+      console.log('Amazing')
+    } catch (error) {
+      console.log('Error in password generator code')
+    }
+  }
+
+  console.log(generate) 
+
   return (
     <div className={`h-screen p-6 flex justify-center items-center static pr-[290px] pb-40 ${isloggedin ? '' : 'blur-background'}`}>
       <div className="fixed right-10 top-2">
         {!isloggedin ? (
-          <div className="w-20 h-[45px] bg-white rounded-md shadow-lg flex items-center justify-center translate-x-[-20px] top-[20px] translate-y-[20px]">
+          <div className="w-20 h-[45px] bg-white rounded-md shadow-lg flex items-center justify-center translate-x-[-50px] top-[20px] translate-y-[20px]">
             <div
               className="text-neutral-800 cursor-pointer"
-              onClick={() => setShowLoginForm(true )}
-               
+              onClick={() => setShowLoginForm(true)}
             >
-              Sign in
+              Sign In
             </div>
           </div>
         ) : null}
-        {isloggedin ? (
-          <div className="w-20 h-[45px] bg-white rounded-md shadow-lg flex items-center justify-center translate-x-[-20px] top-[20px] translate-y-[20px]">
-            <div className="text-neutral-800 cursor-pointer">
-              Logged In
-            </div>
+        {isloggedin && (
+          <div className="w-10 h-10 rounded-full bg-black cursor-pointer hover:opacity-40 hover:scale-105 transform transition-transform translate-y-[25px] translate-x-[-15px]">
+            <AiOutlineUser onClick={handleUserIconClick} className='translate-x-[7px] translate-y-[6px]' size={25}/>
           </div>
-        ) : null}
-        <div className='fixed top-2 right-10'>
-          {isloggedin ? (
-            <div className='w-20 h-[50px] bg-neutral-800 rounded-md shadow-lg flex items-center justify-center translate-x-[-150px] translate-y-[20px] '>
-              <button className='text-white cursor-pointer' onClick={handlereload} >Log Out</button>
-            </div>
-          ) : null}
-        </div>
+        )}
       </div>
-  
+
       {isloggedin ? (
         <div className="w-[100px] h-[50px] translate-x-[-630px] text-center z-20 pb-[900px] flex items-center welcome-section">
           <p className="text-4xl text-white-900 text-left">Welcome!</p>
@@ -154,29 +177,14 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
           <p className="text-4xl text-white-900 text-left filter blur-md r-200">Welcome!</p>
         </div>
       )}
-  
-      <div className="fixed right-10 top-2">
-        {isloggedin ? (
-          <div className="w-20 h-[45px] bg-white rounded-md shadow-lg flex items-center justify-center translate-x-[-20px] top-[20px] translate-y-[20px]">
-            <div className="text-neutral-800 cursor-pointer">
-              Logged In
-            </div>
-          </div>
-        ) : null}
-        <div className='fixed top-2 right-10'>
-          {isloggedin ? (
-            <div className='w-20 h-[50px] bg-neutral-800 rounded-md shadow-lg flex items-center justify-center translate-x-[-150px] translate-y-[20px] '>
-              <button className='text-white cursor-pointer' onClick={handlereload}>Log Out</button>
-            </div>
-          ) : null}
-        </div>
-      </div>
+
       {showLoginForm ? (
         <div className={`max-w-md w-full p-10 bg-neutral-800 rounded-md shadow-lg ${showLoginForm ? 'blur-content' : ''}`}>
           <div className="flex flex-col gap-4">
             <h2 className="text-3xl mt-[20px] font-semibold mb-20 text-center flex justify-center">
-               <Image className=' translate-y-[-2px] translate-x-[-10px] '  src={spotify} width={40} height={40} alt="" />
-               Login to Spotify</h2>
+              <Image className='translate-y-[-2px] translate-x-[-10px]' src={spotify} width={40} height={40} alt="" />
+              Login to Spotify
+            </h2>
             <div className="flex gap-4 items-center bg-neutral-800 p-2 rounded-md">
               <input
                 type="email"
@@ -196,7 +204,10 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
               <input
                 type="password"
                 value={password}
-                onChange={handlePasswordChange}
+                onChange={(e) => {
+                  handlePasswordChange(e);
+                  // Nie ma potrzeby wywoływania `Generatepassword` tutaj
+                }}
                 placeholder="Enter your password"
                 className={`px-6 py-2 bg-neutral-800 text-white rounded-md focus:outline-none transition ${passwordError ? 'border-red-500' : ''}`}
               />
@@ -206,6 +217,9 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
               >
                 Add Password
               </button>
+            </div>
+            <div className='w-[200px] translate-x-[200px]'>
+              <p className='cursor-pointer hover:underline' onClick={handlegeneratepassword}>Generate Password</p>
             </div>
             {passwordError && (
               <p className="text-red-500">Password must be at least 10 characters long.</p>
@@ -227,20 +241,42 @@ const Firstpage: React.FC<FirstPageProps> = ({}) => {
               </div>
             </div>
             <div className='flex items-center justify-center cursor-pointer hover:underline'>
-            <button onClick={() => router.push('/passwordreset' , {scroll:false})}>Forgotten Password?</button>
-</div>
+              <button onClick={() => router.push('/passwordreset', { scroll: false })}>Forgotten Password?</button>
+            </div>
 
             <button
               onClick={handleLogin}
-              className="mt-5  px-4 py-2 bg-neutral-800 text-white rounded-md focus:outline-none transition border"
+              className="mt-5 px-4 py-2 bg-neutral-800 text-white rounded-md focus:outline-none transition border"
             >
               Login
             </button>
           </div>
         </div>
       ) : null}
+
+      {showuserdetails ? (
+        <div className="fixed right-10 top-2 cursor-pointer transition-all" onClick={() => setShowuserdetails(false)}>
+          <div className="w-[160px] h-[230px] rounded-md shadow-lg pl-[10px] bg-neutral-800 block translate-y-[80px] translate-x-[-15px]">
+            <div className="w-full h-[5px] mb-10 text-left">
+              <div className="hover:bg-neutral-600 cursor-pointer p-2 rounded-lg">Account</div>
+            </div>
+            <div className="w-full h-[5px] mb-10">
+              <div className="hover:bg-neutral-600 cursor-pointer p-2 rounded-lg">Profile</div>
+            </div>
+            <div className="w-full h-[5px] mb-10">
+              <div className="hover:bg-neutral-600 cursor-pointer p-2 rounded-lg">Go Premium</div>
+            </div>
+            <div className="w-full h-[5px] mb-10">
+              <div className="hover:bg-neutral-600 cursor-pointer p-2 rounded-lg">Private Session</div>
+            </div>
+            <div className="w-full h-[5px] mb-10">
+              <div className="hover:bg-neutral-600 cursor-pointer p-2 rounded-lg" onClick={() => window.location.reload()}>Log Out</div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
-              }
+}
 
 export default Firstpage;
--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -1,15 +1,21 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineSearch, AiFillPlayCircle } from 'react-icons/ai';
 import axios from 'axios';
-import Image from 'next/image';
-
+import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { useRouter } from 'next/navigation';
 const Search: React.FC = () => {
   const [searchInput, setSearchInput] = useState('');
   const [accessToken, setAccessToken] = useState('');
-  const [artistID, setArtistID] = useState('');
   const [topTracks, setTopTracks] = useState([]);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(-1);
+  const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(null);
 
+  //router statement
+  
+  const router = useRouter()
+
+   
   // Access tokens
   const CLIENT_ID = '7d408fc77ae8499f82bcbef4bce9be2c';
   const CLIENT_SECRET = 'e7c7b60e02e1400ea4f35b0d3c195c91';
@@ -55,8 +61,6 @@ const Search: React.FC = () => {
       if (artistResponse.status === 200) {
         const artistData = artistResponse.data.artists.items[0];
         if (artistData) {
-          setArtistID(artistData.id);
-
           const tracksResponse = await axios.get(`https://api.spotify.com/v1/artists/${artistData.id}/top-tracks?country=US`, {
             headers: {
               'Authorization': `Bearer ${accessToken}`,
@@ -66,6 +70,10 @@ const Search: React.FC = () => {
           if (tracksResponse.status === 200) {
             const tracksData = tracksResponse.data.tracks;
             setTopTracks(tracksData);
+            setCurrentTrackIndex(-1);
+            if (audioElement) {
+              audioElement.pause();
+            }
           } else {
             console.error('Error fetching top tracks:', tracksResponse.statusText);
           }
@@ -80,21 +88,59 @@ const Search: React.FC = () => {
     }
   };
 
-  const handleInputKeyDown = (event) => {
+  const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       searchMusic();
     }
   };
 
-  const handleInputChange = (event) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(event.target.value);
   };
 
+  const handlePlayMusic = (previewUrl: string, index: number) => {
+    if (audioElement) {
+      audioElement.pause();
+    }
+
+    if (currentTrackIndex !== index) {
+      const newAudioElement = new Audio(previewUrl);
+      newAudioElement.play();
+      setAudioElement(newAudioElement);
+      setCurrentTrackIndex(index);
+
+window.onbeforeunload = () =>{
+  return 'Do you want to exit the page and stop play music?'
+}
+
+    } else {
+      setCurrentTrackIndex(-1);
+      window.onbeforeunload = null
+    }
+  };
+
   return (
-    <div className='text-left w-full h-full bg-black'>
+    <div className='text-left w-full h-full bg-neutral-900'>
       <div className='w-[400px] h-[50px] justify-center'>
-        <div className='relative'>
-          <AiOutlineSearch size={25} className='absolute top-2/4 left-4 transform -translate-y-2/4 text-neutral-400' />
+        
+        <div className='relative flex justify-between    w-[550px] translate-y-[15px]'>
+        <div className="flex items-center pl-[10px]">
+              <button onClick={() => router.back()} className="text-white rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                <RxCaretLeft size={35} />
+              </button>
+              <div className="flex items-center ml-4  ">
+                {/* Twój kod */}
+              </div>
+              <button onClick={() => router.forward()} className="text-white rounded-full bg-black flex items-center justify-center hover:opacity-75 transition ml-2">
+                <RxCaretRight size={35} />
+              </button>
+              <div className="flex items-center ml-2">
+                 
+              </div>
+            
+            </div>
+          <AiOutlineSearch size={25} className='absolute top-2/4 left-[160px] transform -translate-y-2/4 text-neutral-400' />
+          
           <input
             className='pl-[40px] rounded-full bg-neutral-800 w-[400px] h-[50px]'
             type='input'
@@ -106,11 +152,33 @@ const Search: React.FC = () => {
         </div>
         <div className='flex cursor-pointer justify-center '>
           {topTracks.map((track, index) => (
-            <div key={index} className=' items-center justify-between'>
-              <div className='rounded-full '>
-              <img src={track.album.images[0].url} alt={track.name} className='w-130 h-30'  />
+            <div
+              key={index}
+              className='items-center justify-between cursor-pointer p-4 border-b border-neutral-700'
+              onClick={() => handlePlayMusic(track.preview_url, index)}
+            >
+              <div className='rounded-full overflow-hidden w-16 h-16'>
+                <img
+                  src={track.album.images[0].url}
+                  alt={track.name}
+                  className='w-full h-full'
+                />
               </div>
-              <p>{track.name}</p>
+              <div className='ml-4'>
+                <p className='text-white font-semibold'>{track.name}</p>
+                <p className='text-neutral-400 text-sm'>{track.artists[0].name}</p>
+              </div>
+              <div>
+                {currentTrackIndex === index ? (
+                  audioElement && !audioElement.paused ? (
+                    <AiFillPlayCircle size={30} className='text-green-500' />
+                  ) : (
+                    <AiFillPlayCircle size={30} className='text-white' />
+                  )
+                ) : (
+                  <AiFillPlayCircle size={30} className='text-white' />
+                )}
+              </div>
             </div>
           ))}
         </div>
